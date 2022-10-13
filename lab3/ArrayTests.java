7f234617e020c2aaa7b9a0c5e5b62afe8ede082b
Mrat <78581772+mr4tt@users.noreply.github.com>
2022-10-13 12:33:53 -0700

    fixed arrayExamples

--- lab3/ArrayTests.java
+++ lab3/ArrayTests.java
@@ -1,18 +1,35 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.Arrays;
+
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+	public void testReverseInPlace()
+    {
+        int[] input1 = { 3 };
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{ 3 }, input1);
+
+        int[] empty = {};
+        ArrayExamples.reverseInPlace(empty);
+        assertArrayEquals(new int[] {}, empty);
+
+        int[] array = {1, 2, 3, 4};
+        ArrayExamples.reverseInPlace(array);
+        System.out.println(Arrays.toString(array));
+
+        assertArrayEquals(new int[] {4, 3, 2, 1}, array);
 	}
 
 
   @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  public void testReversed()
+  {
+      int[] input1 = { };
+      assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+      int[] one = {3};
+      assertArrayEquals(new int[]{3}, ArrayExamples.reversed(one));
   }
 }
