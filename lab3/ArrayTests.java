077a5a014c4321586f592f408215142039ec8a6b
Edward Wang <37031713+ewang2002@users.noreply.github.com>
2023-01-18 15:29:04 -0800

    fix style

--- lab3/ArrayTests.java
+++ lab3/ArrayTests.java
@@ -5,23 +5,23 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace()
-    {
-        int[] input1 = { 3 };
-        ArrayExamples.reverseInPlace(input1);
-        assertArrayEquals(new int[]{ 3 }, input1);
+  @Test 
+  public void testReverseInPlace()
+  {
+      int[] input1 = { 3 };
+      ArrayExamples.reverseInPlace(input1);
+      assertArrayEquals(new int[]{ 3 }, input1);
 
-        int[] empty = {};
-        ArrayExamples.reverseInPlace(empty);
-        assertArrayEquals(new int[] {}, empty);
+      int[] empty = {};
+      ArrayExamples.reverseInPlace(empty);
+      assertArrayEquals(new int[] {}, empty);
 
-        int[] array = {1, 2, 3, 4};
-        ArrayExamples.reverseInPlace(array);
-        System.out.println(Arrays.toString(array));
+      int[] array = {1, 2, 3, 4};
+      ArrayExamples.reverseInPlace(array);
+      System.out.println(Arrays.toString(array));
 
-        assertArrayEquals(new int[] {4, 3, 2, 1}, array);
-	}
+      assertArrayEquals(new int[] {4, 3, 2, 1}, array);
+  }
 
 
   @Test
